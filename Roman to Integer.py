2bc826956b114324684e9d72c226ad3090b21562
HoGyeong Leo Cho <chg8778@gmail.com>
2021-11-26 22:02:47 -0500

    Update Roman to Integer.py

--- Roman to Integer.py
+++ Roman to Integer.py
@@ -62,11 +62,27 @@ Constraints:
 #my initial solution
 class Solution:
     def romanToInt(self, s: str) -> int:
-        symbols = {
+                a = {
             'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
             'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900
         }
-        
+
+        result = 0
+        skip = False
+        if len(s) == 1:
+            return a[s]
+        for i in range(len(s)):
+            if skip:
+                skip = False
+                pass
+            elif i == (len(s)-1):
+                result += a[s[i]]
+            elif a[s[i]] >= a[s[i+1]]:
+                result += a[s[i]]
+            else:
+                result += a[s[i]+s[i+1]]
+                skip = True
+        return result
         
 p = Solution()
 print(p.romanToInt('MCMXCIV'))
