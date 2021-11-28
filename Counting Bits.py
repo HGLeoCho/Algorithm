065bbbec6722eb5540f172f2c63a5c7d8aa80cd3
HoGyeong Leo Cho <chg8778@gmail.com>
2021-11-28 18:36:02 -0500

    Update Counting Bits.py

--- Counting Bits.py
+++ Counting Bits.py
@@ -33,44 +33,31 @@ Follow up:
 
 # my creation for decimal to binary
 def my_bin(num):
-    if not num:
-        return num
-    result = ''
-    copy_num = num
-    while copy_num:
-        if copy_num == 1:
-            result += ('1')
-            copy_num = 0
-        elif copy_num%2 == 1:
-            result += ('1')
-            copy_num //= 2
-        else:
-            result += ('0')
-            copy_num //= 2
-    return result[::-1]
-print(my_bin(16))
-
-# binary conversion I found on stack overflow
-# problem is 0 is not represented as 0
-def decimalToBinary(value):
-    if value < 0:  # Base case if number is a negative
+    if num < 0:
         return 'Not positive'
-    elif value == 0:  # Base case if number is zero
-        return ''
+    elif num == 0:
+        return '0'
+    elif num == 1:
+        return '1'
     else:
-        return decimalToBinary(value // 2) + str(value % 2)
+        return my_bin(num // 2) + str(num % 2)
 
 class Solution:
     def countBits(self, n: int) -> list[int]:
         result = [0]
-        total = n
         if n == 0:
             return result
-        for i in range(n):
-            temp_bits = '0'
-            if not total == 0:
-                total -= 1
-                if temp_bits[:-1] == 1:
-                    temp_bits.append('0')
-                else:
-                    temp_bits[:-1] = '1'
+        for i in range(1, n + 1):
+            result.append(my_bin(i).count('1'))
+        return result
+
+
+p = Solution()
+print(p.countBits(5))  # [0,1,1,2,1,2]
+print(p.countBits(0))  # [0]
+print(p.countBits(10))  # [0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2]
+                  
+'''
+Runtime: 408 ms, faster than 5.03% of Python3 online submissions for Counting Bits.
+Memory Usage: 21 MB, less than 36.06% of Python3 online submissions for Counting Bits.
+'''
