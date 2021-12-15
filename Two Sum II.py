9bddc4a7621e59e7a005abf52ecb4d109a017e01
HoGyeong Leo Cho <chg8778@gmail.com>
2021-12-14 23:44:00 -0500

    Update Two Sum II.py

--- Two Sum II.py
+++ Two Sum II.py
@@ -47,3 +47,19 @@ class Solution:
                     result.append(j)
                     break
         return result
+# ^ O(n^2) solution. not adequate
+       
+'''
+binary search way
+    for i in xrange(len(numbers)):
+        l, r = i + 1, len(numbers) - 1
+        tmp = target - numbers[i]
+        while l <= r:
+            mid = l + (r - l) // 2
+            if numbers[mid] == tmp:
+                return [i + 1, mid + 1]
+            elif numbers[mid] < tmp:
+                l = mid + 1
+            else:
+                r = mid - 1
+'''
