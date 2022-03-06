116c8d36a3a68fa77ccf62b82f41de89c93b9ec4
HoGyeong Leo Cho <chg8778@gmail.com>
2022-03-05 19:47:17 -0800

    Add files via upload

--- LeetCode/src/com/company/Main.java
+++ LeetCode/src/com/company/Main.java
@@ -1,7 +1,5 @@
 package com.company;
 
-import javax.swing.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
@@ -39,8 +37,13 @@ public class Main {
         /*
         Product of Array Except Self
          */
-        ProductofArrayExceptSelf pr = new ProductofArrayExceptSelf();
-        System.out.println(Arrays.toString(pr.solution(new int[] {1,2,3,4})));
+//        ProductofArrayExceptSelf pr = new ProductofArrayExceptSelf();
+//        System.out.println(Arrays.toString(pr.solution(new int[] {1,2,3,4})));
+
+        /* Maximum Subarray */
+        MaximumSubarray ms = new MaximumSubarray();
+        System.out.println(ms.solution2(new int[] {5,4,-1,7,8}));
+        System.out.println(ms.solution2(new int[] {-2,1,-3,4,-1,2,1,-5,4}));
     }
 }
 
@@ -255,9 +258,8 @@ Constraints:
     }
 }
 
-
 class ProductofArrayExceptSelf{
-    public static int[] solution(int[] nums){
+    public int[] solution(int[] nums){
         /*
        1 [1,2,3,4] 1
          [1,1,2,6]
@@ -288,5 +290,73 @@ class ProductofArrayExceptSelf{
         System.out.println(Arrays.toString(result));
 
         return result;
+    }
+
+}
+
+class MaximumSubarray{
+    /*
+    Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+    A subarray is a contiguous part of an array.
+
+
+    Example 1:
+    Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+    Output: 6
+    Explanation: [4,-1,2,1] has the largest sum = 6.
+
+    Example 2:
+    Input: nums = [1]
+    Output: 1
+
+    Example 3:
+    Input: nums = [5,4,-1,7,8]
+    Output: 23
+
+    Constraints:
+    1 <= nums.length <= 105
+    -104 <= nums[i] <= 104
+
+     */
+
+    // quadratic solution. TIME LIMIT EXCEEDED. O(N^2)
+    protected int solution1(int[] nums){
+        int max = Integer.MIN_VALUE;
+        int temp;
+        for(int i = 0; i < nums.length; i++){
+            temp = 0;
+            for(int k = i; k < nums.length; k++){
+                temp += nums[k];
+                max = Math.max(temp, max);
+            }
+        }
+        return max;
+    }
+
+    // 5,4,-1,7,8
+    public int solution2(int[] nums){
+        System.out.println(nums.length);
+        int l = 0;
+        int r = 1;
+        int max_profit = Integer.MIN_VALUE;
+        int sum = nums[l];
+
+        while(l < nums.length){
+            r = l + 1;
+            while(r < nums.length){
+                sum += nums[r];
+                if (max_profit < sum) {
+                    max_profit = sum;
+                }
+                System.out.println(sum + " " + max_profit + " " + l + " " + r);
+                if(r >= nums.length - 1){
+                    break;
+                } else
+                    r++;
+            }
+            l++;
+            sum = nums[l];
+        }
+        return max_profit;
     }
 }
